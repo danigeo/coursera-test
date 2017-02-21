ef5dcac9df96d80366725f972b70353eeff5d67e
dani <danigeo@me.com>
2017-02-22 00:09:11 +0100

    Forth assignment

---
+++ module4-solution/src/components/categories/menuCategories.controller.js
@@ -0,0 +1,15 @@
+(function () {
+  "use strict";
+
+  angular.
+    module("MenuApp").
+    controller("MenuCategoriesController", CategoriesController);
+
+  CategoriesController.$inject = ["categories"];
+  function CategoriesController(categories) {
+    var ctrl = this;
+    ctrl.categories = categories;
+    
+  }
+
+})();
